270ac45a35b997183807cfc2519953c9e521d63b
Yash <tripathiyash143@gmail.com>
2024-08-21 11:17:32 +0530

    Merging Array

---
+++ c3_ArrayMerge.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Merge Array</title>
+    </head>
+        <body>
+            <pre>
+                <?php
+                $a1=array("red","green");
+                $a2=array("blue","yellow");
+                print_r(array_merge($a1,$a2));
+                ?>
+            </pre>
+
+        </body>
+ 
+</html>
