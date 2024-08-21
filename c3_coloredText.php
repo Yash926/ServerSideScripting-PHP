230ffb5dc67598042d11882c9adad2a86d129ca5
Yash <tripathiyash143@gmail.com>
2024-08-21 10:53:52 +0530

    changing text color

---
+++ c3_coloredText.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <title>
+            Colored text in php
+        </title>
+        <style>
+            .red{
+                color:red;
+            }
+            .blue{
+                color:blue;
+            }
+            .green{
+                color:green;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Colored text with PHP</h1>
+        <?php
+        echo '<p class="red">This text is red.</p>';
+        echo '<p class="blue">This text is red.</p>';
+        echo '<p class="green">This text is red.</p>';
+        ?>
+    </body>
+</html>
