8beb057a0b61ba960ed9cb8d3de03e54442da7b5
Yash <tripathiyash143@gmail.com>
2024-08-26 11:55:07 +0530

    Different types of arrays

---
+++ c4_Arrays.php
@@ -0,0 +1,23 @@
+<?php
+//indexed Array
+$courses=array("PHP","Laravel","Node.js","C++","Java");
+// echo "I Like "$courses[0].;
+$courses_length=count($courses);
+for($x=0;$x<$courses_length;$x++)
+{
+    echo $courses[$x];
+    echo "<br>";
+}
+
+//Associative Array
+
+$course1=array("INT220"=>"PHP","INT221"=>"Laravel","INT222"=>"Node JS");
+echo "INT220 is ".$course1['INT220'].". INT221 is ".$course1['INT221']. ". INT222 is ".$course1['INT222'];
+
+$course2["INT220"]="PHP";
+$course2["INT221"]="Laravel";
+$course2["INT222"]="Node js";
+echo "<br>";
+print_r($course2);
+?>
+
