cfe8340353a2df6b18aa33e6a04123a7c0fa705a
Yash <tripathiyash143@gmail.com>
2024-08-28 11:37:40 +0530

    Array functions update

--- c5_ArrayFunctions.php
+++ c5_ArrayFunctions.php
@@ -20,4 +20,38 @@ $a1=array("a"=>"red","b"=>"green","c"=>"blue","d"=>"yellow");
 $a2=array("e"=>"red","f"=>"green","g"=>"blue");
 $result=array_diff($a1,$a2);
 print_r($result);
+
+echo "<br>";
+//Array_flip-function flips/exchanges all keys with their associated values in an array.
+$result1=array_flip($a1);
+print_r($result1);
+echo "<br>";
+
+//Array_intersect-function compares the values of 2 or more arrays, and returns the matches.
+$a3=array("red","blue");
+$result2=array_intersect($a1,$a2,$a3);
+print_r($result2);
+echo "<br>";
+
+//Array_POP-function deletes the last element of an array.
+$a4=array("red","blue","black");
+array_pop($a3);
+print_r($a4);
+echo "<br>";
+
+//array_reverse-returns an array in the reverse order
+$a5=array("a"=>"Volvo","b"=>"BMW","c"=>"Toyota");
+print_r(array_reverse($a5));
+
+echo "<br>";
+//array_search-dearch an array for  a value and return the key.
+echo array_search("Volvo",$a5);
+echo "<br>";
+
+//array_slice()-function returns selected parts of an array.
+
+$a6=array("a"=>"red","b"=>"green","c"=>"blue","d"=>"yellow");
+print_r(array_slice($a6,1,2));
+echo "<br>";
+print_r(array_slice($a6,1,2,true));
 ?>
