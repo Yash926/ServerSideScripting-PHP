3c218c2d104c13fc10a21e32d232be27e19b8b43
Yash <tripathiyash143@gmail.com>
2024-09-02 10:23:20 +0530

    Default argument in function

--- c5_Functions.php
+++ c5_Functions.php
@@ -6,11 +6,18 @@
  }
  sum(3,4);
  
- //loosely typed language
- function add(int $a,int $b)
+ //loosely typed language //works well on w3compiler
+//  function add(int $a,int $b)
+//  {
+//     echo ($a+$b);
+//  }
+//  sum(3,"3 semesters");
+
+ //default argument value
+ function setch(int $child=0)
  {
-    echo ($a+$b);
- }
- sum(3,"3 semesters");
+    echo "Total no of children are : $child <br>" ;
+}
+setch();
  
  ?>
