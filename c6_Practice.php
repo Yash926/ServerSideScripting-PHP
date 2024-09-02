12714651554fb60d8cab1743aaa16e04f40abbbe
Yash <tripathiyash143@gmail.com>
2024-09-02 11:48:31 +0530

    Practice questions

---
+++ c6_Practice.php
@@ -0,0 +1,39 @@
+<?php
+// $x = "test";
+// $y = "this";
+// $z = "also";
+// $x .= $y .= $z;
+// echo $x;
+// echo $y;
+
+// $x = 1;
+// $y = 1;
+// if($result=($x==1)&&($y!=0)){
+//     print "$result";
+// }
+
+
+// $y = 2;
+// if(--$y==2 ||$y xor --$y){
+//     echo $y;
+// }
+
+$x = 1;
+$y = 2;
+if(++$x==$y++){
+    echo "true",$y,$x;
+}
+
+?> 
+
+
+
+
+
+
+
+
+
+
+
+
