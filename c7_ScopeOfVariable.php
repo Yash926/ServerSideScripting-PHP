2482d25887227b99376d441f044f2f68bfebf73f
Yash <tripathiyash143@gmail.com>
2024-09-04 11:11:31 +0530

    Comaprison of static and non static

--- c7_ScopeOfVariable.php
+++ c7_ScopeOfVariable.php
@@ -1,12 +1,34 @@
 <?php
-function myTest(){
-    static $x=1;
-    echo $x;
-    $x++;
+//Static Variable
+
+// function myTest(){
+//     // static $x=1;
+//     $x=1;
+//     echo $x;
+//     $x++;
+// }
+// myTest();
+// echo "<br>";
+// myTest();
+// echo "<br>";
+// myTest();
+
+//Comaprison of static and non static
+function Static_var()
+{
+    static $num1=2;
+    $num2=5;
+    $num1++;
+    $num2++;
+
+    echo "Static: ".$num1."<br>";
+    echo "NonStatic: ".$num2."<br>";
 }
-myTest();
-echo "<br>";
-myTest();
-echo "<br>";
-myTest();
+Static_var();
+Static_var();
+Static_var();
+Static_var();
+Static_var();
+Static_var();
+
 ?>
