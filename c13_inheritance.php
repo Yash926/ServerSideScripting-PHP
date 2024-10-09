3ce9d2abe855c9769d2a5d91228c98f60e67326f
Yash <tripathiyash143@gmail.com>
2024-10-09 11:18:50 +0530

    PHP inheritance

---
+++ c13_inheritance.php
@@ -0,0 +1,35 @@
+<?php
+class Animal{
+    public $name;
+    public $type;
+
+public function __construct($name,$type){
+    $this->name=$name;
+    $this->type=$type;
+}
+
+public function introduce(){
+    echo "I am a ".$this->type." and my name is ".$this->name. ".<br>";
+}
+}
+
+// Define a child class that inherits from animal
+class Dog extends Animal{
+    public function bark(){
+        echo $this->name." says: Woof! Woof!<br>";
+    }
+}
+class Cat extends Animal{
+    public function meow(){
+        echo $this->name." says: Meow!<br>";
+    }
+}
+
+$dog=new Dog("Buddy", "Dog");
+$dog->introduce();
+$dog->bark();
+
+$cat=new Cat("Whiskers", "Cat");
+$cat->introduce();
+$cat->meow();
+?>
